7dff69dfd34f27b5d531f9032ff88efe7cfc48ab
val_boulot <valentin.gallerand@icloud.com>
2025-12-03 17:50:48 +0100

    changes

--- init.lua
+++ init.lua
@@ -882,20 +882,23 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'scottmckendry/cyberdream.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
+      require('cyberdream').setup {
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
+        transparent = false,
+        terminal_colors = true,
+        saturation = 1,
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'cyberdream'
     end,
   },
 
